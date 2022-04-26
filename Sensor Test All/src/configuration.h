6c49ddb8215f2a5aa09877fb2860d87c22a92b9c
MaxlGao <100160681+MaxlGao@users.noreply.github.com>
2022-04-26 10:11:44 -0700

    Initial commit

---
+++ Sensor Test All/src/configuration.h
@@ -0,0 +1,45 @@
+#pragma once
+
+//The default is hotspot mode, the note will be connected to the configured WiFi
+// #define SOFTAP_MODE
+//Sensor selection, the default is the onboard sensor
+// #define __HAS_BME280__               //BME280 temperature, humidity, pressure, height sensor
+// #define __HAS_RGB__                  //WS2812 single point colorful light
+// Only one of the following two can be selected, otherwise there will be conflicts
+// #define __HAS_SHT3X__                //SHT3X temperature and humidity sensor
+// #define __HAS_DS18B20__              //DS18B20 temperature and humidity sensor
+// Wireless access point ssid password
+#define WIFI_SSID               "SETUP-8801"
+#define WIFI_PASSWD             "filter5872chose"
+
+
+#define I2C_SDA                 (25)
+#define I2C_SCL                 (26)
+#define DHT12_PIN               (16)
+#define BAT_ADC                 (33)
+#define SALT_PIN                (34)
+#define SOIL_PIN                (32)
+#define BOOT_PIN                (0)
+#define POWER_CTRL              (4)
+#define USER_BUTTON             (35)
+#define DS18B20_PIN             (21)                  //18b20 data pin
+
+#define RGB_PIN                 (18)
+
+#define OB_BH1750_ADDRESS       (0x23)
+#define OB_BME280_ADDRESS       (0x77)
+#define OB_SHT3X_ADDRESS        (0x44)
+
+#ifdef _USE_CN_
+
+#else
+
+#define DASH_DHT_TEMPERATURE_STRING     "DHT1x Temperature"
+#define DASH_DHT_HUMIDITY_STRING        "DHT1x Humidity"
+#define DASH_BH1750_LUX_STRING          "BH1750"
+#define DASH_SOIL_VALUE_STRING          "Soil Value"
+#define DASH_SALT_VALUE_STRING          "Salt Value"
+#define DASH_BATTERY_STRING             "Battery"
+
+
+#endif  /*_USE_EN_*/
